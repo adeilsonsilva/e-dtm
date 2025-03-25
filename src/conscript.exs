5c71303b24574b630cd05f42a4e7aa2fdb45beb5
Adeilson Silva <adeilson@protonmail.com>
2025-03-25 17:51:12 -0300

    feat: add conscript connection to captain

--- src/conscript.exs
+++ src/conscript.exs
@@ -1,1 +1,11 @@
-Process.sleep(30000)
+Code.require_file("/home/captain.exs") # TODO: learn how to compile stuff with mix
+# require Captain
+
+IO.puts("Hello from #{inspect(Node.self)}.")
+
+
+Captain.enlist(Node.self)
+
+Process.sleep(10000)
+
+Captain.get()
